4b600e92d0427616a11960caf855fb68f3c27d68
vyaron <vyaron@gmail.com>
2024-10-19 19:25:50 +0300

    debug voice

--- js/services/util.service.js
+++ js/services/util.service.js
@@ -1,7 +1,29 @@
 
-
-
 export function speak(txt) {
+  if ('speechSynthesis' in window) {
+    const utterance = new SpeechSynthesisUtterance(txt);
+    let voices = speechSynthesis.getVoices();
+    if (voices.length === 0) {
+      alert('got 0')
+      speechSynthesis.onvoiceschanged = () => {
+        alert('voice-changed')
+        voices = speechSynthesis.getVoices();
+        utterance.voice = voices[0];
+        speechSynthesis.speak(utterance);
+      };
+    } else {
+      utterance.voice = voices[0];
+      alert('about to speak')
+      speechSynthesis.speak(utterance);
+    }
+  } else {
+    alert('SpeechSynthesis is not supported in this browser');
+  }
+}
+
+
+
+export function speak1(txt) {
 
   try {
     // Create a SpeechSynthesisUtterance
